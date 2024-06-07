ee0ea64bab06a204591304afe4db5e4642623de9
POO <andresftma@gmail.com>
2024-06-07 11:26:39 -0500

    Creacion repositorio

--- practica/src/Bancos/CuentaBancaria.java
+++ practica/src/Bancos/CuentaBancaria.java
@@ -1,4 +1,22 @@
 package Bancos;
 
 public class CuentaBancaria {
+    int numCuenta;
+    double saldo;
+    public CuentaBancaria(int numCuenta,double saldo){
+        this.numCuenta=numCuenta;
+        this.saldo=saldo;
+    }
+
+    public int getNumCuenta() {
+        return numCuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+    public void depositar(double monto){
+
+    }
+    public void retirar(double monto){}
 }
